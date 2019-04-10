5dd43a7b237a5e9552730544de03f0458322efd2
polina-okhai <polina.okhay@gmail.com>
2019-04-11 00:08:43 +0300

    Array

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
-  $street = 'Monitorna';
-  $district = "Parus {$street}";   //интерполяция
-  echo($district);
+  $products = ['car', 'house', 'tv'];
+  $phones = [
+    'iphone' => 'Xs',
+    'meizu' => 'M3s'
+  ];
+  $phones['samsung'] = 's9';
+  unset($phones['iphone']);
+  $phones['meizu'] = 'M10';
+  var_dump($phones);
  ?>
+
+ <!-- $street = 'Monitorna';
+ $district = "Parus {$street}";   //интерполяция
+ echo($district); -->
+
   <!-- $name = 'Polina';
   // echo ($name);
   $bool_value = true;
